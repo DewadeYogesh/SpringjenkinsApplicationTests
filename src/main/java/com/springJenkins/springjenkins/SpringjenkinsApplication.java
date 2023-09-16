e7cb4132498165ecfe5293d1426cc8b5071300a9
yogesh <yogeshdewade860@gmail.com>
2023-09-16 10:21:21 +0530

    simple controller added for testing

--- src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
+++ src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
@@ -5,8 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping("home")
 public class SpringjenkinsApplication {
 	public static Logger logger= LoggerFactory.getLogger(SpringjenkinsApplication.class);
 @PostConstruct
@@ -18,5 +21,8 @@ public class SpringjenkinsApplication {
 	logger.info("Application Executed");
 		SpringApplication.run(SpringjenkinsApplication.class, args);
 	}
-
+@GetMapping("welcome")
+	public String getMessage(){
+		return "welcome you successed";
+	}
 }
