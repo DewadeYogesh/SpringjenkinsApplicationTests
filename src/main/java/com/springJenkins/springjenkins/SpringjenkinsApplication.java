952f462592de406258cfe921fac8f4cd328790a5
yogesh <yogeshdewade860@gmail.com>
2023-09-16 10:31:07 +0530

    organised imports

--- src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
+++ src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
@@ -1,6 +1,5 @@
 package com.springJenkins.springjenkins;
 
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
