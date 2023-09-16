cb69db958f544e143139d32171c582cfa7b1a7f8
yogesh <yogeshdewade860@gmail.com>
2023-09-16 10:39:04 +0530

    added

--- src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
+++ src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
@@ -1,5 +1,6 @@
 package com.springJenkins.springjenkins;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
+
 
 @SpringBootApplication
 @RestController
