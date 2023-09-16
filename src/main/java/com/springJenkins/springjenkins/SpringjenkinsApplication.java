c860c6868ba0a391f29b265815f92e3087cfcea8
yogesh <yogeshdewade860@gmail.com>
2023-09-16 10:29:45 +0530

    add restcontroller

--- src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
+++ src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @RequestMapping("home")
 public class SpringjenkinsApplication {
 	public static Logger logger= LoggerFactory.getLogger(SpringjenkinsApplication.class);
