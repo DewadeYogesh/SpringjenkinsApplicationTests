a8cd62438d51ef1ac3431952476f90fd53e75637
yogesh <yogeshdewade860@gmail.com>
2023-09-16 10:36:26 +0530

    added

--- src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
+++ src/main/java/com/springJenkins/springjenkins/SpringjenkinsApplication.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 @RequestMapping("home")
 public class SpringjenkinsApplication {
 	public static Logger logger= LoggerFactory.getLogger(SpringjenkinsApplication.class);
-/*@PostConstruct
+ @PostConstruct
 	public void init(){
 		logger.info("Application Started");
-	}*/
+	}
 
 	public static void main(String[] args) {
 	logger.info("Application Executed");
